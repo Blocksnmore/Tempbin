9324a63d3122532ab5bc7c31c51aaea78a0f086b
Blocksnmore <Blocksnmorereal@gmail.com>
2020-12-03 16:10:31 +0000

    Bug fixes

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ app.post("/*", async (req, res) => {
   const path = req.path.substring(1);
   if (path !== "paste") return res.redirect("/");
   let num = await getPasteNum();
-  let jsondata = {
+  let jsondata = await {
     title: req.body.title ? req.body.title : "Untitled paste",
-    paste: req.body.paste,
-    expiretime: Date.now() + expiretime,
+    paste: req.body.paste+"",
+    expiretime: Date.now() + expiretime
   };
   await db.set("pastedata." + num, jsondata);
   res.redirect("/" + num);
@@ -44,8 +44,13 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+{ const http = require("http");  
+  setInterval(() => { http.get(`http://Tempbin.blocksnmore.repl.co`); }, 280000);
+}
+
 app.listen(process.env.port, async () => {
   let allPastes = await db.list();
+  //db.empty();
   console.log(allPastes) // Debugging to make sure pastes get deleted
   allPastes.forEach(async (paste) => {
     let pastedata = await db.get(paste);
